4c058c044951d0721fa7b2d4b9c64ea67d2b4873
John Abed <john.abed@hotmail.com>
2025-03-30 23:37:35 -0400

    code

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/johnabed/johnabed.github.io/app/src/app/layout.tsx
+// File: /Users/johnabed/johnabed.github.io/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/johnabed/johnabed.github.io/app/src/app/page.tsx
+// File: /Users/johnabed/johnabed.github.io/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
